0f51b175dedcbd684f3a8920a857af38dbe53098
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-10-25 01:04:48 -0700

    uninstalled axios and cleaned up config filee

--- next.config.js
+++ next.config.js
@@ -1,42 +1,37 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
-
 module.exports = {
   async headers() {
     return [
       {
-        source: '/api/task', // ここにAPIエンドポイントのパスを指定
+        source: '/api/task', 
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*', // ワイルドカード(*)を使用してすべてのオリジンからのリクエストを許可
+            value: '*', 
           },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS', // 許可するHTTPメソッドを指定
+            value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS', 
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Authorization', // 許可するヘッダーを指定
+            value: 'X-Requested-With, Content-Type, Authorization', 
           },
         ],
       },
       {
-        source: '/api/task/${id}', // ここにAPIエンドポイントのパスを指定
+        source: '/api/task/${id}', 
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*', // ワイルドカード(*)を使用してすべてのオリジンからのリクエストを許可
+            value: '*', 
           },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS', // 許可するHTTPメソッドを指定
+            value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS', 
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Authorization', // 許可するヘッダーを指定
+            value: 'X-Requested-With, Content-Type, Authorization', 
           },
         ],
       },
